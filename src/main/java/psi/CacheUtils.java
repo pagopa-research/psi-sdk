12fea3f76b09b27309a38cb8f4535f017309bf8d
ianni@lockless.it <v0qq8t2e>
2022-02-10 17:55:14 +0100

    Documented PsiCacheProvider and PsiCacheUtils

--- src/main/java/psi/CacheUtils.java
+++ src/main/java/psi/CacheUtils.java
@@ -10,28 +10,33 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Optional;
 
+/**
+ * This class represents a decoupling layer between the external cache implementation and the facilities required
+ * by the algorithms, providing a set of methods that hide the translation of objects passed to the cache into
+ * cacheable values.
+ */
 class CacheUtils {
 
     private CacheUtils() {}
 
     /**
-     * Retrieves the keyId corresponding to the keyDescription is present, otherwise a new keyId is generated and stored.
-     *
-     * @param keyDescription key (public or private) used by the encryption function.
-     * @param encryptionCacheProvider encryption cache implementation.
-     *
-     * @return the keyId corresponding to the keyDescription
+     * Retrieves the keyId corresponding to the keyDescription if present, otherwise a new keyId is generated and stored.
+     * @param keyDescription    object containing the keys used by the encryption function
+     * @param psiCacheProvider  cache provider implementation
+     * @return the keyId corresponding to the input keyDescription
      */
-    static Long getKeyId(PsiKeyDescription keyDescription, PsiCacheProvider encryptionCacheProvider) {
+    static Long getKeyId(PsiKeyDescription keyDescription, PsiCacheProvider psiCacheProvider) {
         String base64KeyDescription = CustomTypeConverter.convertObjectToString(keyDescription);
         try {
+            // A digest of the keyDescription is used to link a keyId to the provided keyDescription.
+            // In this way the sdk can transparently notice when a keyDescription is reused, exploiting the cache content.
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             String keyDescriptionDigest = new String(messageDigest.digest(base64KeyDescription.getBytes()));
             Optional<String> cachedValue =
-                    encryptionCacheProvider.get(keyDescriptionDigest);
+                    psiCacheProvider.get(keyDescriptionDigest);
             if(!cachedValue.isPresent()){
                 Long keyId = (new SecureRandom()).nextLong();
-                encryptionCacheProvider.put(keyDescriptionDigest, keyId.toString());
+                psiCacheProvider.put(keyDescriptionDigest, keyId.toString());
                 return keyId;
             }
             return Long.parseLong(cachedValue.get());
@@ -40,10 +45,28 @@ class CacheUtils {
         }
     }
 
+    /**
+     * Starting from all the input parameters involved into the encrypted value computation, builds a single string that
+     * can be used as key of the cache element.
+     * @param keyId             id associated to the keyDescription
+     * @param cacheObjectType   enum representing an operation type
+     * @param input             input value of the encryption operation
+     * @return a string that uniquely represents the input parameters
+     */
     private static String generateKeyString(Long keyId, CacheOperationType cacheObjectType, BigInteger input){
         return keyId + cacheObjectType.toString() + CustomTypeConverter.convertBigIntegerToString(input);
     }
 
+    /**
+     * Retrieves the value mapped to the input parameters (keyId, cacheObjectType, input).
+     * @param keyId                 id associated to the keyDescription
+     * @param cacheObjectType       enum representing an operation type
+     * @param input                 input value of the encryption operation
+     * @param typeParameterClass    class type of the object to be retrieved
+     * @param psiCacheProvider      cache provider implementation
+     * @return an Optional containing the cached value corresponding to the input triple (keyId, cacheObjectType,
+     * input), or Optional.empty() if the cache does not contains any value associated to these parameters.
+     */
     static <T> Optional<T> getCachedObject(Long keyId, CacheOperationType cacheObjectType, BigInteger input, Class<T> typeParameterClass, PsiCacheProvider psiCacheProvider){
         String key = generateKeyString(keyId, cacheObjectType, input);
         Optional<String> cachedValueBase64 = psiCacheProvider.get(key);
@@ -53,6 +76,15 @@ class CacheUtils {
         return Optional.of(cachedObject);
     }
 
+    /**
+     * If the cache does not contains any value associated to the triple (keyId, cacheObjectType, input), stores it with
+     * the specified input value, otherwise the state of the cache does not change.
+     * @param keyId             id associated to the keyDescription
+     * @param cacheObjectType   enum representing an operation type
+     * @param input             input value of the encryption operation
+     * @param output            output value of the encryption operation
+     * @param psiCacheProvider  cache provider implementation
+     */
     static void putCachedObject(Long keyId, CacheOperationType cacheObjectType, BigInteger input, CacheObject output, PsiCacheProvider psiCacheProvider){
         String key = generateKeyString(keyId, cacheObjectType, input);
         String value = CustomTypeConverter.convertObjectToString(output);
--- src/main/java/psi/cache/PsiCacheProvider.java
+++ src/main/java/psi/cache/PsiCacheProvider.java
@@ -2,23 +2,27 @@ package psi.cache;
 
 import java.util.Optional;
 
+/**
+ * An interface representing a key-value cache used by the sdk to store the results of heavyweight mathematical
+ * operations, in order to use these results when requested again with the same inputs (e.g. operation type, keys).
+ * Since the cache is used to reduce the computational time required, the provided service must guarantee fast responses.
+ * Since the cache must serve concurrently by multiple requests, its implementations has to be thread safe.
+ */
 public interface PsiCacheProvider {
 
     /**
      * Retrieves the value to which the specified key is mapped.
-     *
      * @param key the key whose associated value is to be returned
-     *
      * @return an Optional containing the cached value to which the specified key is mapped,
-     *          or Optional.empty() otherwise if the cache contains no mapping for the key
+     *          or Optional.empty() if the cache contains no mapping for the key
      */
     public Optional<String> get(String key);
 
     /**
-     * If the specified key is not present into the cache, stores it linked to the input value.
-     *
-     * @param key key with which the specified value is to be associated
-     * @param value alue to be associated with the specified key
+     * If the specified key is not contained into the cache, stores it with the specified input value,
+     * otherwise the state of the cache does not change.
+     * @param key   key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
      */
     public void put(String key, String value);
 }
