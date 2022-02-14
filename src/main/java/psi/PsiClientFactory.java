3974acc09df394a46afc086ce1f1ee990e617dff
ianni@lockless.it <v0qq8t2e>
2022-02-14 17:49:19 +0100

    Fully documented PsiClientFactory

--- src/main/java/psi/PsiClientFactory.java
+++ src/main/java/psi/PsiClientFactory.java
@@ -9,19 +9,31 @@ import psi.model.PsiClientSession;
 
 import java.util.Arrays;
 
+/**
+ * This class offers to the user a generic interface to load a specific PsiClient implementation and configuration
+ * depending on the provided inputs (client session, key description and cache provider).
+ */
 public class PsiClientFactory {
 
     private PsiClientFactory() {}
 
     /**
-     * Creates the specific client object based on the algorithm field defined in the input psiClientSession
+     * Creates the specific client object based on the algorithm field defined in the input psiClientSession.
+     * @param psiClientSession containing the information sent from the server to correctly initialize the client
+     * @return a PsiClient instance configured respect to the input parameter
+     * @throws UnsupportedKeySizeException if the specified key size is not supported for the selected algorithm
      */
     public static PsiClient loadSession(PsiClientSession psiClientSession) throws UnsupportedKeySizeException {
         return loadSessionInner(psiClientSession, null, null);
     }
 
     /**
-     * Creates the specific client object based on the algorithm field defined in the input psiClientSession
+     * Creates the specific client object based on the algorithm field defined in the input psiClientSession, and
+     * externally setup the psiClientKeyDescription used by the computation.
+     * @param psiClientSession          containing the information sent from the server to correctly initialize the client
+     * @param psiClientKeyDescription   containing the information used to perform encryption operations (e.g. exponent of the private key)
+     * @return a PsiClient instance configured respect to the input parameters
+     * @throws UnsupportedKeySizeException if the specified key size is not supported for the selected algorithm
      */
     public static PsiClient loadSession(PsiClientSession psiClientSession, PsiClientKeyDescription psiClientKeyDescription) throws UnsupportedKeySizeException {
         if (psiClientKeyDescription == null) {
@@ -31,6 +43,14 @@ public class PsiClientFactory {
         return loadSessionInner(psiClientSession, psiClientKeyDescription, null);
     }
 
+    /**
+     * Creates the specific client object based on the algorithm field defined in the input psiClientSession. Since a
+     * PsiCacheProvider implementation is passed, the cache support in automatically enabled.
+     * @param psiClientSession  containing the information sent from the server to correctly initialize the client
+     * @param psiCacheProvider  implementation of the psiCacheProvider
+     * @return a PsiClient instance configured respect to the input parameters
+     * @throws UnsupportedKeySizeException if the specified key size is not supported for the selected algorithm
+     */
     public static PsiClient loadSession(PsiClientSession psiClientSession, PsiCacheProvider psiCacheProvider) throws UnsupportedKeySizeException {
         if (psiCacheProvider == null)
             throw new PsiClientException("Input psiCacheProvider is null");
@@ -38,6 +58,16 @@ public class PsiClientFactory {
         return loadSessionInner(psiClientSession, null, psiCacheProvider);
     }
 
+    /**
+     * Creates the specific client object based on the algorithm field defined in the input psiClientSession, and
+     * externally setup the psiClientKeyDescription used by the computation. Since a PsiCacheProvider implementation is
+     * passed, the cache support in automatically enabled.
+     * @param psiClientSession          containing the information sent from the server to correctly initialize the client
+     * @param psiClientKeyDescription   containing the information used to perform encryption operations (e.g. exponent of the private key)
+     * @param psiCacheProvider          implementation of the psiCacheProvider
+     * @return a PsiClient instance configured respect to the input parameters
+     * @throws UnsupportedKeySizeException if the specified key size is not supported for the selected algorithm
+     */
     public static PsiClient loadSession(PsiClientSession psiClientSession, PsiClientKeyDescription psiClientKeyDescription, PsiCacheProvider psiCacheProvider) throws UnsupportedKeySizeException {
         if (psiClientKeyDescription == null)
             throw new PsiClientException("Input clientKeyDescription is null");
@@ -49,7 +79,14 @@ public class PsiClientFactory {
     }
 
     /**
-     * Creates the specific client object based on the algorithm field defined in the input psiClientSession
+     * Creates the specific client object based on the algorithm field defined in the input psiClientSession. If a
+     * PsiCacheProvider implementation is passed, the cache support in automatically enabled. It is not offered to the
+     * user and is used by loadSession methods to select the correct PsiClient implementation.
+     * @param psiClientSession          containing the information sent from the server to correctly initialize the client
+     * @param psiClientKeyDescription   containing the information used to perform encryption operations (e.g. exponent of the private key)
+     * @param psiCacheProvider          implementation of the psiCacheProvider
+     * @return a PsiClient instance configured respect to the input parameters
+     * @throws UnsupportedKeySizeException if the specified key size is not supported for the selected algorithm
      */
     private static PsiClient loadSessionInner(PsiClientSession psiClientSession, PsiClientKeyDescription psiClientKeyDescription, PsiCacheProvider psiCacheProvider) throws UnsupportedKeySizeException {
         if (!Arrays.asList(PsiAlgorithm.values()).contains(psiClientSession.getPsiAlgorithmParameter().getAlgorithm()))
